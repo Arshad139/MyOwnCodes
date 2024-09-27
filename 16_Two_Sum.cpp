baaff5eea53f8b67acd41b874a66c206fe22e1a1
Arshad139 <97882067+Arshad139@users.noreply.github.com>
2024-09-27 07:09:28 +0530

    Update 16_Two_Sum.cpp

--- 16_Two_Sum.cpp
+++ 16_Two_Sum.cpp
@@ -8,12 +8,33 @@ public:
         for(auto i=0;i<len;i++) {
             int diff=target-nums[i];
             if(hash.count(diff)) 
-                 return {i,hash[target-nums[i]]};
+                 return {i,hash[diff]};
             else 
                hash[nums[i]]=i;
         
         }
          return {-1};      
         }
+
+// The below is one of the straight forward approaches. has a worst case complexity of O(n2), iterate over the array by 
+//assuming that the current element is one of the required ones & check if any of the other elements in the array, combined
+// with the current element, if the condition is met, then return the indices by breaking the loop.
+vector<int> initialTwoSum(vector<int>& nums, int target) {
+        int i=0,j=0;
+        int len = nums.size();
+        vector<int> ans(2, 0); 
+        for (i = 0; i < len; i++){
+            for(j= i+1 ; j< len; j++){
+                if(nums[i] + nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                    break;
+                }
+                    
+            }
+        }
+        return ans;
+        
+    }
                                    
 };
