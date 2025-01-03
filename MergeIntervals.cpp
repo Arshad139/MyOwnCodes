bf36e527573bacf6ee7e6ce4f8717b1f82dcf5fa
Arshad139 <97882067+Arshad139@users.noreply.github.com>
2025-01-03 09:57:23 +0530

    Update MergeIntervals.cpp

--- MergeIntervals.cpp
+++ MergeIntervals.cpp
@@ -3,6 +3,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+ //    int merged = 0;
+	// int n = arr.size();
+	// do{
+	// 	merged = 0;
+ //        for (int i = 1; i < n; i++) {
+	// 		if(arr[i][0] <= arr[i-1][1]){
+	// 			arr[i-1][1] = arr[i][1];
+	// 			arr.erase(arr.begin()+i);
+	// 			n--;
+	// 			merged++;
+	// 		}
+ //        }
+	// } while(merged > 0);
+ //    return arr;
+        
+        
+        //The above was your previous approach, but the below one looks great enough, to start with, you will need the intervals to be in a sorted order. 
+        //Then you use an output array, where you keep pushing the merged intervals, the first interval goes in first & then you iterate over the arrays by comparing
+        //the elements & thus by updating the previous elemets upper bound - this is what you call the merging operation. There was no question of index being out of range too. 
         int n = intervals.size();
         vector<vector<int>> ans;
 
